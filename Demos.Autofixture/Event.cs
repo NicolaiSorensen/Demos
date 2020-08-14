b9d5f227e8641362e26e1891afd77bf47ad15947
NiSø <nicolai.sorensen@gmail.com>
2020-08-14 10:20:39 +0200

    Simple tests showing the problem
    Add test

---
+++ Demos.Autofixture/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace Demos.AutoFixture
+{
+    public class Event
+    {
+        public Event(string subject, string content)
+        {
+            Subject = subject ?? throw new ArgumentNullException(nameof(subject));
+            Content = content ?? throw new ArgumentNullException(nameof(content));
+            Created = DateTime.Now;
+        }
+
+        public string Subject { get; private set; }
+        public string Content { get; private set; }
+        public DateTime Created  { get; private set; }
+    }
+}
