46ebb852df0b16ea029ad128252149995cb724e4
NiSø <nicolai.sorensen@gmail.com>
2020-08-14 12:03:24 +0200

    Simple EventDispatcher Test

---
+++ Demos.AutoFixture.Tests/EventDispatcherTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AutoFixture.Xunit2;
+using FluentAssertions;
+using Xunit;
+
+namespace Demos.AutoFixture.Tests
+{
+    public class EventDispatcherTests
+    {
+        public class SendAll
+        {
+            [Theory]
+            [AutoData]
+            public void Should_send_all_valid_events(EventDispatcher sut, Event[] events)
+            {
+                sut.SendAll(events);
+
+
+            }
+    }
+    }
+}
