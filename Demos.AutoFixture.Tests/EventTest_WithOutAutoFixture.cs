55307910a6074819ec483a88d54f911fa957ac37
NiSø <nicolai.sorensen@gmail.com>
2020-09-15 23:35:10 +0200

    Add  EventTest_WithoutAutoFixture

---
+++ Demos.AutoFixture.Tests/EventTest_WithOutAutoFixture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Demos.AutoFixture.Tests
+{
+    public class EventTest_WithOutAutoFixture
+    {
+        public class Constructor
+        {
+            [Fact]
+            public void Should_set_Created_to_now()
+            {
+                var sut = new Event("testSubject", "testContent");
+
+                sut.Created.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(1));
+            }
+
+            [Fact]
+            public void Should_set_subject()
+            {
+                var sut = new Event("testSubject", "testContent");
+
+                sut.Subject.Should().BeEquivalentTo("testSubject");
+            }
+
+            [Fact]
+            public void Should_set_content()
+            {
+                var sut = new Event("testSubject", "testContent");
+
+                sut.Content.Should().BeEquivalentTo("testContent");
+            }
+        }
+    }
+}
